d169fdd7461e63ee1ec31ab34ab0e6ffec79fb91
JDSeiler <jorsei1p217@gmail.com>
2019-11-24 15:20:22 -0500

    Add methods for getting and setting karma values in the database

--- src/db-util.js
+++ src/db-util.js
@@ -3,6 +3,18 @@ const sqlite3 = require('sqlite3').verbose();
 class DB {
     constructor(path) {
         this.path = path;
+        this.tableName = "userKarma"
+        this.initTable();
+    }
+    
+    initTable() {
+        let db = this.openDatabase();
+        const initString = `CREATE TABLE IF NOT EXISTS ${this.tableName}(
+            tag TEXT, 
+            karma INTEGER,
+            UNIQUE(tag));`;
+        db.run(initString);
+        db.close();
     }
 
     openDatabase() {
@@ -15,63 +27,20 @@ class DB {
         });
         return db;
     }
-    
-    initTable(tableName) {
-        let db = this.openDatabase();
-        this.tableName = tableName;
-        const initString = `CREATE TABLE IF NOT EXISTS ${this.tableName}(
-            tag TEXT, 
-            karma INTEGER,
-            UNIQUE(tag));`;
-        db.run(initString);
-        db.close()
-    }
 
-    // Wont take a callback as a parameter for some reason?
-    queryKarma(userTag) {
+    queryUserKarma(user, callback) {
         let db = this.openDatabase();
-        let queryString = `SELECT karma FROM ${this.tableName} WHERE tag = "${userTag}";`;
-        const result = db.get(queryString, (err, row) => {
-            if (err) {
-                console.log(err);
-                return;
-            } else {
-                if (row === undefined) {
-                    console.log("No results");
-                } else {
-                    return row;
-                }
-            }
-        });
+        let queryString = `SELECT karma FROM userKarma WHERE tag IS '${user}';`;
+        db.get(queryString, callback);
         db.close();
     }
 
-    addUser(userTag) {
+    setUserKarma(user, newKarma, callback) {
         let db = this.openDatabase();
-        let initString = `INSERT OR IGNORE INTO ${this.tableName} VALUES("${userTag}", 0);`
-        db.run(initString);
+        let updateString = `UPDATE userKarma SET karma = ${newKarma} WHERE tag IS '${user}';`;
+        db.run(updateString, callback);
         db.close();
     }
-
-    increaseKarma(userTag) {
-        let db = this.openDatabase();
-        // Not working because async is painful
-        let newKarma = this.queryKarma("Jeff") + 1;
-        console.log(newKarma);
-        // Cannot update the karma if you can't get the karma
-        // I need a method to get the karma out of the database safely every time or the rest
-        // of this project will never work.
-        const updateString = `UPDATE ${this.tableName} SET karma = ${newKarma} WHERE tag = '${userTag}'`;
-        const output = db.run(updateString, (result, err) => {
-            if (err) {
-                console.log(err);
-                return
-            } else {
-                console.log(result)
-            }
-        });
-        db.close();
-    }
-}
+}  
 
 module.exports = DB
--- src/karma-tracker.js
+++ src/karma-tracker.js
@@ -9,21 +9,33 @@ class KarmaTracker extends emitter {
     // TODO: Actually develop
     collectVotes(messageReaction) {
         let db = new DB("/home/jordan/Projects/bash-bot/src/db/test.db");
-        db.initTable("userKarma");
-        db.addUser("Jeff");
-        db.increaseKarma("Jeff");
-        db.queryKarma("Jeff");
-        const author = messageReaction.message.author;
+        const authorID = messageReaction.message.author.id;
         const reaction = messageReaction.emoji.identifier;
         // TODO: Move into configurable file
         const upVote = "%E2%AC%86%EF%B8%8F";
         const downVote = "%E2%AC%87";
-        console.log(reaction);
-
+        const userName = "Dave";
+        db.queryUserKarma(authorID, (err, row) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // Checking against a string because undeinfed can be overwritten, typeof cannot
+                if (typeof row !== 'undefined') {
+                    console.log("Karma is " + row.karma);
+                } else {
+                    console.log("No DB entry found for: " + authorID);
+                }
+            }
+        });
+        db.setUserKarma(userName, 20, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("No fatal errors (does not indicate success)");
+            }
+        });
         if (reaction === upVote) {
             console.log("Upvoted!");
-                // db.increaseKarma("Jeff");
-            // db.queryKarma("Jeff");
         } else if (reaction === downVote) {
             console.log("Downvoted!");
         } else {
