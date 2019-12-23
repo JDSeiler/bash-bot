6282b903fac99cb1d534063408be44f7445b76fa
JDSeiler <jorsei1p217@gmail.com>
2019-12-23 16:34:58 -0500

    Add help documentation for commands thus far

--- src/commands/fetch.js
+++ src/commands/fetch.js
@@ -3,6 +3,10 @@ const DB = require('../db-util.js');
 module.exports = {
     name: "fetch",
     desc: "Return the karma of the specified user",
+    help: "**Fetch**:\n\
+`Usage: $fetch @user1 [@users]`\n\
+Command takes 1 or more user mentions as arguments, seperated by spaces. Output is the karma of each\
+ mentioned user. If a user is not yet in the database, their karma will be listed as `undefined`\n",
     async printUserKarma(message, user) {
         const db = await new DB("./src/db/test.db");
         const userKarma = await db.queryUserKarma(user.id);
--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,6 +1,8 @@
 module.exports = {
     name: "ping",
     desc: "Ping Pong!",
+    help: 
+    "**Ping**:\nTest command that responds with \"Pong.\" to the user who calls it.\n",
     execute(message, args) {
         message.channel.send('Pong.');
     }
