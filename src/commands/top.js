6b5604ed81ef7b47da3f73987fc95e646e0afcdd
JDSeiler <jorsei1p217@gmail.com>
2020-01-03 15:58:03 -0500

    Add user documentation and possible argument to top

--- src/commands/top.js
+++ src/commands/top.js
@@ -2,39 +2,69 @@ const DB = require('../db-util.js');
 
 module.exports = {
     name: "top",
-    desc: "(dummy docs) Display the top users by karma",
-    help: "TODO",
-
+    desc: "Display a specified number of users, sorted by karma. Or all users (up to 25) if no number is given.",
+    help: "**Top**:\n\
+`Usage: $top [numberOfUsersToFetch]`\n\
+Command takes an optional whole number argument. If no number is given, all of the users in the karma \
+database will be displayed, sorted by karma (up to a maximum of 25). Otherwise, display the number \
+of users specified, sorted by karma.",
 
     async execute(message, args) {
         const db = new DB("./src/db/test.db");
         const results = await db.queryTopUsers();
         const guild = message.guild;
         if (guild.available) {
-            for (const row of results) {
-                try {
-                    const guildMember = await this.resolveID(guild, row.tag);
-                    if (guildMember) {
-                        message.channel.send(`${guildMember.user.username} has ${row.karma} karma.`);
+            if (args.length === 1) {
+                const numberOfUsersToDisplay = Number(args[0]);
+                if (await this.validateArgument(numberOfUsersToDisplay) == true) {
+                    if (numberOfUsersToDisplay >= results.length) {
+                        this.displayMembers(message, guild, results);
                     } else {
-                        message.channel.send(`${row.tag} cannot be resolved into a server member.`);
+                        const resultsSlice = results.slice(0, numberOfUsersToDisplay);
+                        this.displayMembers(message, guild, resultsSlice);
                     }
-                } catch (err) {
-                    console.log(err);
+                } else {
+                    message.reply(`${numberOfUsersToDisplay} is not a valid argument`);
                 }
+            } else {
+                this.displayMembers(message, guild, results);
             }
         } else {
             message.reply("Something went wrong when accessing this guild!").catch(console.log(error));
         }
     },
 
+    async displayMembers(message, guild, databaseResults) {
+        for (const row of databaseResults) {
+            try {
+                const guildMember = await this.resolveID(guild, row.tag);
+                if (guildMember) {
+                    message.channel.send(`${guildMember.user.username} has ${row.karma} karma.`);
+                } else {
+                    message.channel.send(`${row.tag} cannot be resolved into a server member.`);
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    },
+
     async resolveID(guild, id) {
-        try{
+        try {
             const guildMember = await guild.fetchMember(id);
             return guildMember;
         } catch (err) {
             console.log(`Failed to fetch ${id}`);
-            console.log(err);
+        }
+    },
+
+    async validateArgument(number) {
+        if (Number.isNaN(number)) {
+            return false;
+        } else if (Number.isSafeInteger(number) && number > 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
