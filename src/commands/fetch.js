6ac35f01ce46718578877b1d8657b28d34425da3
JDSeiler <jorsei1p217@gmail.com>
2020-01-03 20:23:15 -0500

    Add bunyan logging for 'fetch' command

--- src/commands/fetch.js
+++ src/commands/fetch.js
@@ -1,25 +1,43 @@
 const DB = require('../db-util.js');
+const path = require('path');
+const bunyan = require('bunyan');
+
+const logger = bunyan.createLogger({
+    name: "index.js",
+    streams: [{
+        path: path.resolve("./log/combined.log")
+    },
+    {
+        stream: process.stdout
+    }],
+    src: true
+})
 
 module.exports = {
     name: "fetch",
     desc: "Return the karma of the specified user",
     help: "**Fetch**:\n\
 `Usage: $fetch @user1 [@users]`\n\
-Command takes 1 or more user mentions as arguments, seperated by spaces. Output is the karma of each\
- mentioned user. If a user is not yet in the database, their karma will be listed as `undefined`\n",
+Command takes 1 or more user mentions as arguments, seperated by spaces. Output is the karma of each \
+mentioned user. If a user is not yet in the database, their karma will be listed as `undefined`\n",
+
     async printUserKarma(message, user) {
-        const db = await new DB("./src/db/test.db");
-        const userKarma = await db.queryUserKarma(user.id);
-        let karmaValue;
-        if (typeof userKarma == 'undefined') {
-            karmaValue = 'undefined';
-        } else {
-            karmaValue = userKarma.karma
+        try {
+            const db = await new DB("./src/db/test.db");
+            const userKarma = await db.queryUserKarma(user.id);
+            let karmaValue;
+            if (typeof userKarma == 'undefined') {
+                karmaValue = 'undefined';
+            } else {
+                karmaValue = userKarma.karma
+            }
+            message.channel.send(`${user.tag}'s karma is ${karmaValue}`);
+        } catch (err) {
+            logger.warn(err);
         }
-        message.channel.send(`${user.tag}'s karma is ${karmaValue}`);
     },
 
-    async execute(message, args) {
+    async execute(message, _) {
         const users = message.mentions.users;
         for (let entry of users.entries()) {
             const user = entry[1];
